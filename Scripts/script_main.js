dcde77cd0b62fd363a31436ea129f027435cf7cd
slatalison <xiaos157@newschool.edu>
2020-03-09 18:58:48 -0400

    基本layout完成

--- Scripts/script_main.js
+++ Scripts/script_main.js
@@ -1,11 +1,5 @@
-let page;
-let pages = ["animation_page", "make_tea", "drink_tea"];
-var $nextButton12 = $(".nextButton12");
-var $nextButton23 = $(".nextButton23");
-var $animation_page = $(".animation_page");
-var $drink_tea = $(".drink_tea");
-var $make_tea = $(".make_tea");
-var $restart = $(".restart");
+// let page;
+// let pages = ["animation_page", "make_tea", "drink_tea"];
 
 // function switchPage() {
 //   for (page = 0; page < pages.length; page++) {
@@ -14,12 +8,19 @@ var $restart = $(".restart");
 //   }
 // }
 
-
 // function getPage(){
 //   let pageName = document.querySelector('.' + pages[0]);
 //   let style = getComputedStyle(pageName);
 // };
 
+//切换三个页面
+//reference https://codepen.io/rashidnaushad/pen/VaKMBJ
+var $nextButton12 = $(".nextButton12");
+var $nextButton23 = $(".nextButton23");
+var $animation_page = $(".animation_page");
+var $drink_tea = $(".drink_tea");
+var $make_tea = $(".make_tea");
+var $restart = $(".restart");
 
 $nextButton12.click(function () {
   $make_tea.fadeIn();
@@ -39,3 +40,102 @@ $nextButton23.click(function () {
 $restart.click(function () {
   location.reload(true);
 });
+
+//animation页take user input
+$(document).ready(function () {
+  // Get value on button click and show alert
+  $(".nextButton12").click(function () {
+    var str = $("#playerName").val();
+    console.log("Hello " + str + " ~");
+  });
+
+});
+
+
+
+//drag and drop
+//reference https://codepen.io/byronglover/pen/oxjgEK
+
+//用→切换ingredient视图
+var ig_container1 = document.getElementById("ig_container1");
+var ig_container2 = document.getElementById("ig_container2");
+
+function showHide() {
+  if (ig_container1.style.display !== "none") {
+    ig_container1.style.display = "none";
+    ig_container2.style.display = "block";
+    console.log("Page 1 here");
+  } else {
+    ig_container1.style.display = "block";
+    ig_container2.style.display = "none";
+    console.log("Page 2 here");
+  }
+};
+
+//make tea页批量创建div以及load image
+const ingredient_images = [
+  "blacktea-1.png",
+  "blacktea-2.png",
+  "blacktea.png",
+  "bubble cup.png",
+  "cane sugar.png",
+  "cheese.png",
+  "chilli powder.png",
+  "fruits.png",
+  "greentea-1.png",
+  "greentea-2.png",
+  "greentea-3.png",
+  "greentea-4.png",
+  "greentea.png",
+  "ice.png",
+  "pencil powder.png",
+  "vinegar.png",
+  "water.png",
+  "whipped cream.png",
+  "whole milk-1.png",
+  "whole milk-2.png",
+  "whole milk-3.png",
+  "whole milk.png"
+];
+
+let imgSwitch = 0;
+let row1 = document.getElementById("row1");
+let row2 = document.getElementById("row2");
+let row3 = document.getElementById("row3");
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadImages1()
+});
+
+loadImages1 = () => {
+  for (let i = 0; i < 6; i++) {
+    let newImg = document.createElement('img');
+    newImg.className = "ingredient_images";
+    newImg.id = "ingredientNo" + i;
+    newImg.src = "visual/" + ingredient_images[i];
+    row1.appendChild(newImg);
+  };
+  for (let i = 6; i < 10 ; i++) {
+    let newImg = document.createElement('img');
+    newImg.className = "ingredient_images";
+    newImg.id = "ingredientNo" + i;
+    newImg.src = "visual/" + ingredient_images[i];
+    row2.appendChild(newImg);
+  };
+  for (let i = 10; i < 14 ; i++) {
+    let newImg = document.createElement('img');
+    newImg.className = "ingredient_images";
+    newImg.id = "ingredientNo" + i;
+    newImg.src = "visual/" + ingredient_images[i];
+    row3.appendChild(newImg);
+  };
+  for (let i = 14; i < 19 ; i++) {
+    let newImg = document.createElement('img');
+    newImg.className = "ingredient_images";
+    newImg.id = "ingredientNo" + i;
+    newImg.src = "visual/" + ingredient_images[i];
+    row4.appendChild(newImg);
+  };
+
+};
+
